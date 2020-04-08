e182597987e719d27af6b681cc8733f065b548aa
Robin_Yao_Wenbin <wenbin_yao@foxmail.com>
2020-04-08 20:30:08 +0800

    Add files via upload

--- python业务代码/apriori.py
+++ python业务代码/apriori.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 08 16:43:21 2020
+Created on Wed Apr 08 17:43:21 2020
 
 @author: wenbin
 
@@ -77,7 +77,45 @@ def apriori(dataSet, minSupport = 0.5):
 
  
 ####################################################################################
- # 关联规则挖掘
+ # 关联规则挖掘(来源于网络的代码,关联规则挖掘这一部分是错误的)
+
+# 获取关联规则的封装函数
+def generateRules(L, supportData, minConf=0.7):  # supportData 是一个字典
+    bigRuleList = []
+    for i in range(1, len(L)):  # 从为2个元素的集合开始
+        for freqSet in L[i]:
+            # 只包含单个元素的集合列表
+            H1 = [frozenset([item]) for item in freqSet]    # frozenset({2, 3}) 转换为 [frozenset({2}), frozenset({3})]
+            # 如果集合元素大于2个，则需要处理才能获得规则
+            if (i > 1):
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf) # 集合元素 集合拆分后的列表 。。。
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+ 
+# 对规则进行评估 获得满足最小可信度的关联规则
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []  # 创建一个新的列表去返回
+    for conseq in H:
+        conf = supportData[freqSet]/supportData[freqSet-conseq]  # 计算置信度
+        if conf >= minConf:
+            print(freqSet-conseq,'-->',conseq,'conf:',conf)
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+ 
+# 生成候选规则集合
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)): # 尝试进一步合并
+        Hmp1 = aprioriGen(H, m+1) # 将单个集合元素两两合并
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 1):    #need at least two sets to merge
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+
+#############################################################################
+
+###原来的关联规则函数是错误的,没有考虑到所有情况,因此我重写了一个.
 
 def calSupport(ab , dataSet):
     count = 0
@@ -110,8 +148,7 @@ def geneRule(L, supportData,dataSet , minConf=0.7 ):
                             print(L[i][k] , " -> " , L[j][m] , " , confidence = " , tmp)
 
 
-#####################################################################################
-# 数据整理及主函数
+#############################################################################
 
 def splitstr(x):
     x = x[1:-1]
